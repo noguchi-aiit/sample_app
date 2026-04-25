80f4cebad5bf4ae349035af3cdc79a999a6ec03e
noguchi-aiit <a26123kn@aiit.ac.jp>
2026-04-25 05:01:23 +0000

    change home and help html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    #app/views/static_pages/home.html.erbにデフォルトは動作
   end
 
   def help
+    #app/views/static_pages/help.html.erbにデフォルトは動作
   end
 end
