8c2220480d51a3142489bd02957b16b27f4ec303
noguchi-aiit <a26123kn@aiit.ac.jp>
2026-04-25 06:23:29 +0000

    FInish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  # GET/ が実行されたら以下を実行
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  root 'hello#index'
 end
