ab1b7db6807f456492bc0449d5eb0c0549435442
noguchi-aiit <a26123kn@aiit.ac.jp>
2026-05-22 08:36:07 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to user_url(@user)
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
@@ -22,6 +24,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.expect(user: [:name, :email, :password,
-                                  :password_confirmation])
+                           :password_confirmation])
     end
 end
